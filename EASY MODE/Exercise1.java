0c2b8774cccd8347c3a25ef3b06bdf2befb00f1c
Cristina <cristinamateospaez@gmail.com>
2023-08-29 14:28:14 +0200

    First attempt

---
+++ EASY MODE/Exercise1.java
@@ -0,0 +1,43 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class Exercise1 {
+
+    /**
+     * Given an integer array nums, return true if any value appears 
+     * at least twice in the array, and return false if every element is distinct.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5}; 
+        Exercise1 exercise = new Exercise1();
+        System.out.println("Contains duplicates: " + exercise.containsDuplicate(nums));
+        System.out.println("Contains duplicates2: " + exercise.containsDuplicate2(nums));
+    }    
+
+    public boolean containsDuplicate(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            System.out.println(nums[i] + " Bucle Externo");
+            for(int j=i+1; j<nums.length; j++) {
+                System.out.println(nums[j] + " Bucle Interno");
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate2(int[] nums) {
+        Set<Integer> seenNumbers = new HashSet<>();
+        for (int num : nums) {
+            if (seenNumbers.contains(num)) {
+                System.out.println(num + "Duplicate Number");
+                return true;
+            }
+            seenNumbers.add(num);
+        }
+        return false;
+    }
+}
