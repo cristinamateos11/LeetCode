65fe742ca9ced8b5006ec9753294be8f5ebcee85
Cristina <cristinamateospaez@gmail.com>
2023-09-01 15:43:40 +0200

    Update Exercise4.java

--- EASYMODE/Exercise4.java
+++ EASYMODE/Exercise4.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class Exercise4 {
     
+
+    /**
+     * Given an integer x, return true if x is a palindrome, and false otherwise.
+     * 
+     * Example 1:
+     * Input: x = 121
+     * Output: true
+     * Explanation: 121 reads as 121 from left to right and from right to left
+     * 
+     * Example 2:
+     * Input x = -121
+     * Output: false
+     * Explanation: -121 reads as 121-
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Exercise4 exercise4 = new Exercise4();
@@ -19,8 +35,13 @@ public class Exercise4 {
             return false;
         }
 
+        // convertimos el numero en una cadena de caracteres
         String original = String.valueOf(x);
+
+        // revertimos la cadena
         String reverse = new StringBuilder(original).reverse().toString();
+        
+        // convertimos la cadena invertida en un numero
         int reverseInt = Integer.parseInt(reverse);
         // long reverseLong = Long.parseLong(reverse);
 
