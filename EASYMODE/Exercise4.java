a990640337e56305cacad2bcebf012f1e009663c
Cristina <cristinamateospaez@gmail.com>
2023-09-01 15:40:22 +0200

    New Exercises

--- EASYMODE/Exercise4.java
+++ EASYMODE/Exercise4.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class Exercise4 {
     
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Exercise4 exercise4 = new Exercise4();
+
+        System.out.println("Escribe un número: ");
+        int number = scanner.nextInt();
+
+        System.out.println("¿Es un palíndromo?: " + exercise4.isPalindrome(number));
+        scanner.close();
+
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        String original = String.valueOf(x);
+        String reverse = new StringBuilder(original).reverse().toString();
+        int reverseInt = Integer.parseInt(reverse);
+        // long reverseLong = Long.parseLong(reverse);
+
+        if (x == reverseInt) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
